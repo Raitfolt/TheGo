16c10c05d109e7d1dbd94bf24c26fc827394528e
Антон Новосельцев <raitfolt@gmail.com>
2023-07-31 11:11:17 +0500

    exercise 1.3

--- 1_2/main.go
+++ 1_2/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -37,4 +38,25 @@ func main() {
 	// потенциально неэффективных версий и версии с применением strings.Join.
 	// (В разделе 1.6 демонстрируется часть пакета time , а в разделе 11.4 — как написать
 	// тест производительности для ее систематической оценки.)
+
+	start := time.Now()
+	res1 := ""
+	for i := 0; i < 10000; i++ {
+		res1 = strings.Join(os.Args, " ")
+	}
+	duration := time.Since(start)
+	fmt.Println(res1)
+	fmt.Println(duration)
+
+	start = time.Now()
+	res2 := ""
+	for i := 0; i < 10000; i++ {
+		res2 = ""
+		for _, v := range os.Args {
+			res2 += v + " "
+		}
+	}
+	duration = time.Since(start)
+	fmt.Println(res2)
+	fmt.Println(duration)
 }
