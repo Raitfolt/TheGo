c5ff7326b3bfcb4fcd56b39b40a64a26a3fd4042
Антон Новосельцев <raitfolt@gmail.com>
2023-07-31 16:51:14 +0500

    exercise 1.6

--- ch1/lissajous/lissajous.go
+++ ch1/lissajous/lissajous.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/gif"
@@ -40,6 +41,13 @@ func lissajous(out io.Writer) {
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0
 
+	r := uint8(rand.Intn(256))
+	g := uint8(rand.Intn(256))
+	b := uint8(rand.Intn(256))
+	fmt.Printf("R = %d G = %d B = %d\n", r, g, b)
+	rndColor := color.RGBA{r, g, b, 0xFF}
+	palette = append(palette, rndColor)
+
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 
@@ -48,7 +56,7 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(len(palette)-1))
 		}
 
 		phase += 0.1
