24d31d3d4b9a1a39699e5cb34f7dac1b6bc48d99
Антон Новосельцев <raitfolt@gmail.com>
2023-07-31 15:20:24 +0500

    add files reader

--- 1_3/main.go
+++ 1_3/main.go
@@ -10,7 +10,36 @@ func main() {
 	fmt.Println("1.3")
 
 	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2 : %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
+
+/* input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
 		counts[input.Text()]++
 	}
@@ -19,4 +48,4 @@ func main() {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
-}
+}*/
