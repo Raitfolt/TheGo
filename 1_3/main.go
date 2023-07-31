28fe61bf2d44fa5377080fe2738a27b9685a69af
Антон Новосельцев <raitfolt@gmail.com>
2023-07-31 15:53:27 +0500

    add exercise 1.4

--- 1_3/main.go
+++ 1_3/main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("1.3")
-
 	counts := make(map[string]int)
-	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2 : %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
+	filenames := make(map[string]string)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		// read strings ONLY if they ended by "\n"
+		for _, line := range strings.Split(string(data), "\n") {
+			filenames[filename] = "ok"
+			counts[line]++
 		}
 	}
 
@@ -30,22 +28,5 @@ func main() {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
+	fmt.Println(filenames)
 }
-
-func countLines(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
-}
-
-/* input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
-}*/
