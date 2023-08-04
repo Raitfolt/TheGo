5b4fee69a9373db01e0f46da91edfb30949c963c
Антон Новосельцев <raitfolt@gmail.com>
2023-08-04 15:51:12 +0500

    ready to exercise 4.3 - 4.7

--- ch4/append/append.go
+++ ch4/append/append.go
@@ -19,6 +19,26 @@ func appendInt(x []int, y int) []int {
 	return z
 }
 
+func remove2(slice []int, i int) []int {
+	copy(slice[i:], slice[i+1:])
+	return slice[:len(slice)-1]
+}
+
+func remove(slice []int, i int) []int {
+	copy(slice[1:i+1], slice[:i])
+	return slice[1:]
+}
+
+func reverseOld(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func reverse(slice []int) []int {
+	return slice
+}
+
 func main() {
 	var x, y []int
 	for i := 0; i < 10; i++ {
@@ -26,4 +46,12 @@ func main() {
 		fmt.Printf("%d len=%d  cap=%d  \t%v\n", i, len(y), cap(y), y)
 		x = y
 	}
+
+	s := []int{5, 6, 7, 8, 9}
+	fmt.Println(s)
+	fmt.Println(remove(s, 2))
+	fmt.Println(s)
+	fmt.Println(remove2(s, 2))
+	fmt.Println(s)
+	//123
 }
