a38f53aeca66198d4a326adabce4d4978685e55d
Антон Новосельцев <raitfolt@gmail.com>
2023-08-09 14:18:42 +0500

    iasa

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/Raitfot/TheGo
 
 go 1.20
 
-require golang.org/x/net v0.14.0
+require golang.org/x/net v0.14.0 // indirect
+
+require github.com/Raitfolt/le v0.0.0-20230809063258-b8d3751023f7
--- main.go
+++ main.go
@@ -2,54 +2,35 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"log"
+	"os"
+
+	"github.com/Raitfolt/le"
 )
 
-var prereqs = map[string][]string{
-	"algorithms": {"data structures"},
-	"calculus":   {"linear algebra"},
-
-	"compilers": {
-		"data structures",
-		"formal languages",
-		"computer organization",
-	},
-
-	"data structures":       {"discrete math"},
-	"databases":             {"data structures"},
-	"discrete math":         {"intro to programming"},
-	"formal languages":      {"discrete math"},
-	"networks":              {"operating systems"},
-	"operating systems":     {"data structures", "computer organization"},
-	"programming languages": {"data structures", "computer organization"},
-}
-
 func main() {
-	for i, course := range topoSort(prereqs) {
-		fmt.Printf("%d:\t%s\n", i+1, course)
-	}
+	breadthFirst(crawl, os.Args[1:])
 }
 
-func topoSort(m map[string][]string) []string {
-	var order []string
+func breadthFirst(f func(item string) []string, worklist []string) {
 	seen := make(map[string]bool)
-
-	var visitAll func(items []string)
-	visitAll = func(items []string) {
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
 		for _, item := range items {
 			if !seen[item] {
 				seen[item] = true
-				visitAll(m[item])
-				order = append(order, item)
+				worklist = append(worklist, f(item)...)
 			}
 		}
 	}
+}
 
-	var keys []string
-	for key := range m {
-		keys = append(keys, key)
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := le.Extract(url)
+	if err != nil {
+		log.Print(err)
 	}
-	sort.Strings(keys)
-	visitAll(keys)
-	return order
+	return list
 }
