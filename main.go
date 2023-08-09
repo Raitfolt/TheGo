d6d31d84306f2521109223ab7bf64892872a6b30
Антон Новосельцев <raitfolt@gmail.com>
2023-08-09 09:25:26 +0500

    171(book)

--- main.go
+++ main.go
@@ -2,63 +2,54 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"sort"
 )
 
-func square(n int) int {
-	return n * n
-}
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
 
-func negative(n int) int {
-	return -n
-}
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
 
-func product(m, n int) int {
-	return m * n
-}
-
-func squaresF() func() int {
-	var x int
-	return func() int {
-		x++
-		return x * x
-	}
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
 }
 
 func main() {
-
-	defer func() {
-		if recover() != nil {
-			fmt.Println("Was panic")
-		}
-	}()
-
-	f := square
-	fmt.Println(f(3))
-
-	f = negative
-	fmt.Println(f(3))
-	fmt.Printf("%T\n", f)
-
-	f2 := product
-	fmt.Println(f2(3, 5))
-
-	fmt.Println(strings.Map(add1, "Hello, world"))
-
-	fmt.Println(strings.Map(func(r rune) rune { return r + 2 }, "Hello, world"))
-
-	sF := squaresF()
-	fmt.Println(sF())
-	fmt.Println(sF())
-	fmt.Println(sF())
-	fmt.Println(sF())
-	fmt.Println(sF())
-	fmt.Println(sF())
-	fmt.Println(sF())
-	fmt.Println(sF())
-	fmt.Println(sF())
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
 }
 
-func add1(r rune) rune {
-	return r + 1
+func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+
+	var visitAll func(items []string)
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+
+	var keys []string
+	for key := range m {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	visitAll(keys)
+	return order
 }
